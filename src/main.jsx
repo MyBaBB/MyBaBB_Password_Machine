4d4b1146a2b03a52c84e57d1622b6e6450cfc76c
Brett Baker <GitBaBB@MyBaBB.com>
2024-05-14 09:36:05 -0700

    redid the whole password generator with new code and all,  workiing on the styles now but have to test the gh pagesf first

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Gorilla from "./Components-Folder/GorillaFolder/Gorilla.jsx";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    < Gorilla />
-  </React.StrictMode>,
-);
+    <App />
+  </React.StrictMode>
+)
