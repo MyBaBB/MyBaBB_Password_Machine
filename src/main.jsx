672b478266e6fcb2188c450211f9292d3194821b
Brett Baker <GitBaBB@MyBaBB.com>
2024-05-14 05:38:41 -0700

    under contruction converting to vite

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Gorilla from "./Components-Folder/GorillaFolder/Gorilla.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    < Gorilla />
+  </React.StrictMode>,
+);
