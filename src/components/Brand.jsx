7cb17c6382446b2f47c372b8a72c6861e30c2a30
Brett Baker <GitBaBB@MyBaBB.com>
2024-05-14 15:38:21 -0700

    another complete work over , much cleaner

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -1,14 +1,17 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import { KeyIcon } from "../icons/KeyIcon";
+import { TfiNotepad } from "react-icons/tfi";
 import styles from "../styles/brand.module.css";
-
+import { PiRewindCircleFill } from "react-icons/pi";
 export const Brand = () => {
     return (
         <header className={styles.header}>
+            <a href="https://mybabb.github.io/MyBABB-Remember/">
             <div className={styles.iconContainer}>
-                <KeyIcon />
+            <TfiNotepad size={50} color="blue"/>
+             <span className="absolute top-[3rem] text-green-600"><PiRewindCircleFill size={30}/></span>
             </div>
+            \</a>
             <div className={styles.brandTitle}>
                 <h1>
                    <span className="myName text-blue-900">Brett Baker</span>
