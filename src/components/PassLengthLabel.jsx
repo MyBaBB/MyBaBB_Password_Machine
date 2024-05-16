a5eed9d66e4825dca095862dd3f79f61f88308a1
Brett Baker <GitBaBB@MyBaBB.com>
2024-05-16 03:50:43 -0700

    trying another font

--- src/components/PassLengthLabel.jsx
+++ src/components/PassLengthLabel.jsx
@@ -5,7 +5,7 @@ import styles from "../styles/passLengthLabel.module.css";
 export const PassLengthLabel = ({ length }) => {
     return (
         <div className={styles.container}>
-            <span className="font-ArchivoBlack-Regular"
+            <span className="  font-Rye-Regular text-lg text-gray-800"
              style={{ textShadow: "1px 1px 1px #fff" }}>Password length</span>
             <span className={styles.result}>{length}</span>
         </div>
